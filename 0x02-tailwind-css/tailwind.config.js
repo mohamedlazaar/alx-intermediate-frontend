8b5591ba0fd21f3adc08f9e16a2a6dd1337dba1b
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-12-08 09:37:14 +0100

    Create a Responsive Flexbox Layout with Tailwind

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  "./1-index.html",
-  "./2-index.html",
-  "./3-nav_index.html",
-]
-  ,
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
